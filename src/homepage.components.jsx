23c7c1329780e9a7abe0dba8e2e7585c3ab1cc92
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-13 11:36:07 +0530

    First Commit

---
+++ src/homepage.components.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className="homepage" >
+        <h1>Hello World</h1>
+    </div>
+);
+
+export default HomePage;
