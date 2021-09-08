271ba34e9c181a13e38c18808b7055df9b23cd16
shams-aaliya <shamsaaliya08@gmail.com>
2021-09-08 20:56:55 +0530

    Adding Collection component

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../collection-preview/collection-preview.components';
+
 
 export class shop extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
             collections:SHOP_DATA
         }
     }
-    render() {
+    render() 
+    {
+        
         return (
-            <div>
-                <h1>SHOP</h1>
+            <div className='shop'>
+               {
+                this.state.collections.map(({id, ...otherProps}) => (
+                    <CollectionPreview  key={id} {...otherProps}/>
+                ))
+               }
             </div>
         )
     }
